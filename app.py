b5103d2cf6c89eb88991053675e3d3e3a7ef6d86
MilesTsue93 <65318978+MilesTsue93@users.noreply.github.com>
2023-12-20 00:40:12 +0000

    added a secret key

--- app.py
+++ app.py
@@ -1,41 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    "id": 1,
-    "title": "Data Analyst",
-    "location": "Sacramento, CA",
-    "salary": "$10,000"
-  },
-  {
-    "id": 2,
-    "title": "Data Engineer",
-    "location": "San Mateo, CA"
-  },
-  {
-    "id": 3,
-    "title": "Software Developer",
-    "location": "New York, NY",
-    "salary": "$20,000"
-  },
-  {
-    "id": 4,
-    "title": "Data Scientist",
-    "location": "Merced, CA",
-    "salary": "$210,000"
-  }
-]
 
 @app.route("/")
-def hello_world():
-  return render_template("home.html", jobs=JOBS, company="Miles'")
+def home():
+    return render_template(
+        "home.html",
+        jobs=load_jobs_from_db(),
+        company="Miles'")
+
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+    return jsonify(load_jobs_from_db())
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
